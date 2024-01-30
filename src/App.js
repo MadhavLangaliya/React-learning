60574fb7931060b910f430b5c987dbf37b9add3e
madhav langaliya <madhav.langaliya@trootech.com>
2024-01-30 14:46:45 +0530

    basic structure of app when project create

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
-import React from "react";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-} from "react-router-dom";
-import Home from "./Pages";
-import About from "./Pages/about";
-import Events from "./Pages/events";
-import AnnualReport from "./Pages/annual";
-import Teams from "./Pages/team";
-import Blogs from "./Pages/blogs";
-import SignUp from "./Pages/signup";
+import React from 'react';
+import './App.css';
 
-function App() {
-	return (
-		<Router>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route
-					path="/events"
-					element={<Events />}
-				/>
-				<Route
-					path="/annual"
-					element={<AnnualReport />}
-				/>
-				<Route path="/team" element={<Teams />} />
-				<Route path="/blogs" element={<Blogs />} />
-				<Route
-					path="/sign-up"
-					element={<SignUp />}
-				/>
-			</Routes>
-		</Router>
-	);
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Welcome to My React App</h1>
+      <p>This is a sample React application.</p>
+    </div>
+  );
 }
 
 export default App;
